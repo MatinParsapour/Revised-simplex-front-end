7aca8a27168fadd858cec00e8a6fb9037304c1cc
Matin <matin.parsapour.iam@gmail.com>
2022-08-08 15:20:23 +0430

    Get and save constraints and variables from url whenever it changes

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -9,9 +9,17 @@ export class MainComponent implements OnInit {
   constraints!: number;
   variables!: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.constraints = +this.route.snapshot.queryParams['constraints'];
+    this.variables = +this.route.snapshot.queryParams['variables'];
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.constraints = +queryParams['constraints'];
+      this.variables = +queryParams['variables'];
+      console.log({
+        'Constraints ': this.constraints,
+        'Variables: ': this.variables,
+      });
+    });
   }
 
 }
