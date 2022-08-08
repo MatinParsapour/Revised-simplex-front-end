bf8903d6d07ce91ab0567a6d85652e62fdae46d5
Matin <matin.parsapour.iam@gmail.com>
2022-08-08 15:17:44 +0430

    Add a routing for main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+  {path: 'main', component: MainComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
