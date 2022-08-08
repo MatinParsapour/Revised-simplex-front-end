1981179dd62cc3ac04ce84b467b0a3cd07802034
Matin <matin.parsapour.iam@gmail.com>
2022-08-08 15:19:12 +0430

    Import UserInputs, Matrices and FormsModule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { UserInputsComponent } from './components/user-inputs/user-inputs.component';
+import { MatricesComponent } from './components/matrices/matrices.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
