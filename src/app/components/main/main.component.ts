96714094c2b54191f7e7493b65210e697122c8b9
Matin <matin.parsapour.iam@gmail.com>
2022-08-08 15:20:06 +0430

    Add two property constraints and variables to get and save them from url

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  constraints!: number;
+  variables!: number;
 
   constructor() { }
 
