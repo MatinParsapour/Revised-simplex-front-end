d658e311f3dea2b3fcca88578c9ffa22ee746623
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 15:40:26 +0430

    Update import file typescript file to import file and upload to server

--- src/app/components/import-file/import-file.component.ts
+++ src/app/components/import-file/import-file.component.ts
@@ -1,4 +1,6 @@
+import { FileService } from './../../services/file/file.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-import-file',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./import-file.component.css']
 })
 export class ImportFileComponent implements OnInit {
+  file!: File
+  status!: string;
+  message!: string;
 
-  constructor() { }
+  constructor(private fileService: FileService) { }
 
   ngOnInit(): void {
+  }
+
+  importFile(event: any) {
+    this.file = event.target.files[0];
+  }
+
+  uploadFile(){
+    const formData = this.createFormData(this.file);
+    this.fileService.post("file/import-excel", formData).subscribe(
+      response => {
+        status = "Ok",
+        this.message = "Your file uploaded, wait to calculate"
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    )    
+  }
+
+  private createFormData(file: File): FormData {
+    const formData = new FormData;
+    formData.append("file", file)
+    return formData;
   }
 
 }
