c236cdd7d5ea3ad43f578e2915429c76d01e21a3
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 15:20:38 +0430

    Add import file component to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { MainComponent } from './components/main/main.component';
 import { UserInputsComponent } from './components/user-inputs/user-inputs.component';
 import { MatricesComponent } from './components/matrices/matrices.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ImportFileComponent } from './components/import-file/import-file.component'
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     UserInputsComponent,
-    MatricesComponent
+    MatricesComponent,
+    ImportFileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
