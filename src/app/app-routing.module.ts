5d99413316968f8b3fa5c11c11fe34ebbe22d36d
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 15:53:22 +0430

    Add routing for result component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ResultComponent } from './components/result/result.component';
 import { MainComponent } from './components/main/main.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'},
   {path: 'main', component: MainComponent},
+  {path: 'result', component: ResultComponent},
 ];
 
 @NgModule({
